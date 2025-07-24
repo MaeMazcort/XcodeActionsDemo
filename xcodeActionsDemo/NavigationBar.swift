870798040cadd96e73280a7316d095863c04bc9e
Nina Mazadiego <n_mazadiegocortes@apple.com>
2025-07-23 23:17:21 -0700

    Test 123

--- xcodeActionsDemo/NavigationBar.swift
+++ xcodeActionsDemo/NavigationBar.swift
@@ -15,7 +15,7 @@ struct NavigationBar: View {
             HomeView()
                 .tabItem {
                     Image(systemName: "house.fill")
-                    Text("Test")
+                    Text("Test 123")
                 }
                 .tag(1)
             
